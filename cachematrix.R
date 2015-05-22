fcdd8e47727fefbb04be275499b5e8cbd55250c8
ChrisColvin <chris.colvin@hotmail.com>
2015-05-22 15:17:19 -0700

    Updated code based upon directions for exercise 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script will create an inverse of a matrix and store the inverse in cache.
+## If the matrix, in cache is called, the cache will provide the output.
+## If the matrix is not in cache, the new matrix inverse will be calculated and then cached.
+## Function test patterns
+## 3  5
+## -7 2
+## 5 6
+## 7 8
+## "getting cached data"
+## 5 6
+## 7 8
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This will inverse the matrix and cache the inverse
+makeCacheMatrix <- function(x=matrix()) {
+        minv<- NULL
+        set<- function(y) {
+              x<<- y
+              minv<<- NULL
+        }
+        get<- function() x
+        setinverse<- function(inverse) minv <<- inverse
+        getinverse<- function() minv
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## THis test to see if the matrix is in cache, and if so returns the cache.  If not, it calculates the inverse.
+cacheSolve<- function(x, ...) {
+        minv<- NULL
+        minv<- x$getinverse()
+        if(!is.null(minv)) {
+                message("getting cached data.")
+                return(minv)
+        }
+        data<- x$get()
+        minv<- solve(data, ...)
+        x$setinverse(minv)
+        minv
 }
